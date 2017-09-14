b9d54ff7c7d7de24e2649498bef142270ac1e10a
cheng.cheng <cheng.cheng_2@nxp.com>
2017-09-14 18:31:41 +0800

    Initial commit for serial port.

---
+++ serial-tool/serial_port.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'serial port status'
+
+
+__author__ = 'cc'
+
+import serial
+import serial.tools.list_ports
+
+port_list = list(serial.tools.list_ports.comports())
+
+if(len(port_list) < 0):
+	print('serial port not avaliable')
+else:
+	port_name = [list(x)[0] for x in port_list]
+	print(port_name)
