4c597b73b01a95a6ebc4528491fb7a9fd6c0d4ef
cheng.cheng <cheng.cheng_2@nxp.com>
2017-09-15 18:22:28 +0800

    @nxp 2017/9/15

--- serial-tool/serial_port.py
+++ serial-tool/serial_port.py
@@ -9,18 +9,23 @@ __author__ = 'cc'
 import serial
 import serial.tools.list_ports
 
-port_list = list(serial.tools.list_ports.comports())
+def port_list():
 
-if(len(port_list) < 0):
-	print('serial port not avaliable')
-else:
-	port_name = [list(x)[0] for x in port_list]
-	print(port_name)
+	port_list = list(serial.tools.list_ports.comports())
+
+	if(len(port_list) < 0):
+		print('serial port not avaliable')
+	else:
+		port_name = [list(x) for x in port_list]
+		print('Total com ports %d:\n'%len(port_name))
 
 
-class serial_port(serial):
-	"""docstring for serial_port"""
-	def __init__(self, arg):
-		super(serial_port, self).__init__()
-		self.arg = arg
+
+port_list()
+
+#class serial_port(serial):
+#	"""docstring for serial_port"""
+#	def __init__(self, arg):
+#		super(serial_port, self).__init__()
+#		self.arg = arg
 		
