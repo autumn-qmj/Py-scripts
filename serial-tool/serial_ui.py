23d70899de8360a645cf5bb8de32138f67a4b9f3
salfe <13656229452@163.com>
2017-09-20 23:56:28 +0800

    Implementation of UI.

--- serial-tool/serial_ui.py
+++ serial-tool/serial_ui.py
@@ -7,27 +7,38 @@
 __author__ = 'cc'
 
 from Tkinter import *
+from ttk import *
 
+BaudrateList = [115200, 9600]
 
 class serial_ui(Frame):
 	def __init__(self, parent):
 		Frame.__init__(self, parent)
 		self.pack(fill=BOTH, expand = True)
 
+		#open button
 		self.openText = StringVar()
-		self.sendText = StringVar()
 		self.openText.set('open')
-		buttonOpen = Button(self, text=self.openText.get(), command = self.click_open).grid(column=0, row=0)
-
+		buttonOpen = Button(self, textvariable=self.openText, command = self.click_open).grid(column=0, row=0)
+		#send button
+		self.sendText = StringVar()
 		text = StringVar()
 		entry = Entry(self, width = 20, textvariable = self.sendText).grid(column=0, row=1)
 		buttonSend = Button(self, text='send', command = self.click_send).grid(column=4, row=1)
+		#baudrate combobox
+		baudrate = Combobox(self, values = BaudrateList, state = 'readonly').grid(column=0, row=2).current(0)
 
 
 	def click_open(self):
-		self.openText.set('close')
+		if self.openText.get() == 'open':
+			self.openText.set('close')
+		else:
+			self.openText.set('open')
 
 	def click_send(self):
+		pass
+
+	def setBaudrate(self):
 		pass
 
 
