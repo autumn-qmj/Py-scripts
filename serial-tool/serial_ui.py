53c4968dcc37a79c0d1c0bfc63c8b98a13328c7a
cheng.cheng <cheng.cheng_2@nxp.com>
2017-09-21 19:43:00 +0800

    Improve UI.
    Signed-off-by: cheng.cheng <cheng.cheng_2@nxp.com>

--- serial-tool/serial_ui.py
+++ serial-tool/serial_ui.py
@@ -9,25 +9,64 @@ __author__ = 'cc'
 from Tkinter import *
 from ttk import *
 
-BaudrateList = [115200, 9600]
-
+ConfigurationLabel = ['baudrate', 'bytesize', 'parity', 'stopbits', 'xonxoff', 'rts', 'dtr']
+BaudrateList = [256000, 128000, 115200, 57600, 56000, 38400, 19200, 14400, 9600, 4800, 2400, 1200, 600, 300]
+BytesizeList = [5, 6, 7, 8]
+ParityList = ['None', 'Odd', 'Even', 'Mark', 'Space']
+StopbitsList = ['1', '1.5', '2']
+FlowCtrlList= ['None', 'Software']
+DtrrtsList = ['Disable', 'Enable']
 class serial_ui(Frame):
 	def __init__(self, parent):
 		Frame.__init__(self, parent)
 		self.pack(fill=BOTH, expand = True)
 
+		
+		#send button
+		self.sendText = StringVar()
+		entry = Entry(self, width = 20, textvariable = self.sendText).grid(column=2, row=1)
+		buttonSend = Button(self, text='send', command = self.click_send).grid(column=4, row=1)
+		
+		for index, l in enumerate(ConfigurationLabel):
+			Label(self, text=l).grid(column=0, row=2+index, sticky = 'w')
+
+		#refresh button
+		buttonRefresh = Button(self, text='refresh', command = self.click_refresh).grid(column=0, row=1)
+
 		#open button
 		self.openText = StringVar()
 		self.openText.set('open')
-		buttonOpen = Button(self, textvariable=self.openText, command = self.click_open).grid(column=0, row=0)
-		#send button
-		self.sendText = StringVar()
-		text = StringVar()
-		entry = Entry(self, width = 20, textvariable = self.sendText).grid(column=0, row=1)
-		buttonSend = Button(self, text='send', command = self.click_send).grid(column=4, row=1)
-		#baudrate combobox
-		baudrate = Combobox(self, values = BaudrateList, state = 'readonly').grid(column=0, row=2).current(0)
+		buttonOpen = Button(self, textvariable=self.openText, command = self.click_open).grid(column=1, row=1)
 
+		#baudrate combobox
+		self.baudrate = Combobox(self, values = BaudrateList, state = 'readonly', width = 10)
+		self.baudrate.grid(column=1, row=2)
+		self.baudrate.current(2)
+		#bytesize combobox
+		self.bytesize = Combobox(self, values = BytesizeList, state = 'readonly', width = 10)
+		self.bytesize.grid(column=1, row=3)
+		self.bytesize.current(3)
+		#parity combobox
+		self.parity = Combobox(self, values = ParityList, state = 'readonly', width = 10)
+		self.parity.grid(column=1, row=4)
+		self.parity.current(0)
+		#stopbits combobox
+		self.stopbits = Combobox(self, values = StopbitsList, state = 'readonly', width = 10)
+		self.stopbits.grid(column=1, row=5)
+		self.stopbits.current(0)
+		#flow control combobox
+		self.stopbits = Combobox(self, values = FlowCtrlList, state = 'readonly', width = 10)
+		self.stopbits.grid(column=1, row=6)
+		self.stopbits.current(0)
+		#rts checkbutton
+		self.rts = Combobox(self, values = DtrrtsList, state = 'readonly', width = 10)
+		self.rts.grid(column=1, row=7)
+		self.rts.current(0)
+		#dtr checkbutton
+		self.dtr = Combobox(self, values = DtrrtsList, state = 'readonly', width = 10)
+		self.dtr.grid(column=1, row=8)
+		self.dtr.current(0)
+		
 
 	def click_open(self):
 		if self.openText.get() == 'open':
@@ -38,7 +77,7 @@ class serial_ui(Frame):
 	def click_send(self):
 		pass
 
-	def setBaudrate(self):
+	def click_refresh(self):
 		pass
 
 
