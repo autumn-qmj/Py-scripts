a92982e61d98fd213f6142901c5a0c5a5c586bf5
cheng.cheng <cheng.cheng_2@nxp.com>
2017-09-22 18:32:21 +0800

    Nop.
    Signed-off-by: cheng.cheng <cheng.cheng_2@nxp.com>

--- serial-tool/serial_ui.py
+++ serial-tool/serial_ui.py
@@ -8,6 +8,8 @@ __author__ = 'cc'
 
 from Tkinter import *
 from ttk import *
+import ScrolledText as st
+
 
 ConfigurationLabel = ['baudrate', 'bytesize', 'parity', 'stopbits', 'xonxoff', 'rts', 'dtr']
 BaudrateList = [256000, 128000, 115200, 57600, 56000, 38400, 19200, 14400, 9600, 4800, 2400, 1200, 600, 300]
@@ -24,19 +26,12 @@ class serial_ui(Frame):
 
 		self.confFrame = LabelFrame(self)
 		self.confFrame.grid(row = 0, column = 0, sticky = 'wesn')
-		self.transFrame = LabelFrame(self)
-		self.transFrame.grid(row = 0, column = 1, sticky = 'wesn')
-
-		self.create_configuration_frame()
-		self.create_transfer_frame()
-
-		
-	def create_transfer_frame(self):
-		self.sendframe = LabelFrame(self.transFrame)
+		self.sendframe = LabelFrame(self)
 		self.sendframe.grid(row = 1, column = 1, sticky = 'wesn')
-		self.recvframe = LabelFrame(self.transFrame)
+		self.recvframe = LabelFrame(self)
 		self.recvframe.grid(row = 0, column = 1, sticky = 'wesn')
 
+		self.create_configuration_frame()
 		self.create_send_frame()
 		self.create_recv_frame()
 		
@@ -47,61 +42,67 @@ class serial_ui(Frame):
 		buttonSend = Button(self.sendframe, text='send', command = self.click_send).grid(column=2, row=1)
 
 	def create_recv_frame(self):
-		pass
+		self.recvText = st.ScrolledText(self.recvframe, width = 100, height = 40).grid(column=1, row=1)
 
 	def create_configuration_frame(self):
-		self.confTopFrame = LabelFrame(self.confFrame)
-		self.confTopFrame.grid(row = 0, column = 0, sticky = 'wesn')
-		self.confTBotFrame = LabelFrame(self.confFrame)
-		self.confTBotFrame.grid(row = 1, column = 0, sticky = 'wesn')
-
+		#self.confTopFrame = LabelFrame(self.confFrame)
+		#self.confTopFrame.grid(row = 0, column = 0, padx = 0, pady = 0, sticky = 'n')
+		self.confBotFrame = LabelFrame(self.confFrame)
+		self.confBotFrame.grid(row = 1, column = 0, pady = 0, sticky = 'wesn')
 		self.create_configuration_top_frame()
 		self.create_configuration_bottom_frame()
 
 
 	def create_configuration_top_frame(self):
-		pass
+		self.comList = StringVar()
+		#select mode:browse, extended
+		#func:curselection(), return the current select item
+		#selection_set(index), set item to select
+		#see(index), check the item is avaliable or not
+		#listbox.bind('<<ListboxSelect>>', func)
+		#
+		Listbox(self.confFrame, height = 20, listvariable = self.comList.get(), selectmode = 'browse', bg = 'WhiteSmoke').grid(row = 0, column = 0, sticky = 'wesn')
 
 	def create_configuration_bottom_frame(self):
 		for index, l in enumerate(ConfigurationLabel):
-			Label(self.confTBotFrame, text=l).grid(column=0, row=2+index, sticky = 'w')
+			Label(self.confBotFrame, text=l).grid(column=0, row=2+index, sticky = 'w')
 
 		self.row = 1
 
 		#refresh button
-		buttonRefresh = Button(self.confTBotFrame, text='refresh', command = self.click_refresh).grid(column=0, row=1)
+		buttonRefresh = Button(self.confBotFrame, text='refresh', command = self.click_refresh).grid(column=0, row=1)
 
 		#open button
 		self.openText = StringVar()
 		self.openText.set('open')
-		buttonOpen = Button(self.confTBotFrame, textvariable=self.openText, command = self.click_open).grid(column=1, row=1)
+		buttonOpen = Button(self.confBotFrame, textvariable=self.openText, command = self.click_open).grid(column=1, row=1)
 
 		#baudrate combobox
-		self.baudrate = Combobox(self.confTBotFrame, values = BaudrateList, state = 'readonly', width = 10)
+		self.baudrate = Combobox(self.confBotFrame, values = BaudrateList, state = 'readonly', width = 10)
 		self.baudrate.grid(column=1, row=2)
 		self.baudrate.current(2)
 		#bytesize combobox
-		self.bytesize = Combobox(self.confTBotFrame, values = BytesizeList, state = 'readonly', width = 10)
+		self.bytesize = Combobox(self.confBotFrame, values = BytesizeList, state = 'readonly', width = 10)
 		self.bytesize.grid(column=1, row=3)
 		self.bytesize.current(3)
 		#parity combobox
-		self.parity = Combobox(self.confTBotFrame, values = ParityList, state = 'readonly', width = 10)
+		self.parity = Combobox(self.confBotFrame, values = ParityList, state = 'readonly', width = 10)
 		self.parity.grid(column=1, row=4)
 		self.parity.current(0)
 		#stopbits combobox
-		self.stopbits = Combobox(self.confTBotFrame, values = StopbitsList, state = 'readonly', width = 10)
+		self.stopbits = Combobox(self.confBotFrame, values = StopbitsList, state = 'readonly', width = 10)
 		self.stopbits.grid(column=1, row=5)
 		self.stopbits.current(0)
 		#flow control combobox
-		self.stopbits = Combobox(self.confTBotFrame, values = FlowCtrlList, state = 'readonly', width = 10)
+		self.stopbits = Combobox(self.confBotFrame, values = FlowCtrlList, state = 'readonly', width = 10)
 		self.stopbits.grid(column=1, row=6)
 		self.stopbits.current(0)
 		#rts checkbutton
-		self.rts = Combobox(self.confTBotFrame, values = DtrrtsList, state = 'readonly', width = 10)
+		self.rts = Combobox(self.confBotFrame, values = DtrrtsList, state = 'readonly', width = 10)
 		self.rts.grid(column=1, row=7)
 		self.rts.current(0)
 		#dtr checkbutton
-		self.dtr = Combobox(self.confTBotFrame, values = DtrrtsList, state = 'readonly', width = 10)
+		self.dtr = Combobox(self.confBotFrame, values = DtrrtsList, state = 'readonly', width = 10)
 		self.dtr.grid(column=1, row=8)
 		self.dtr.current(0)
 		
@@ -123,6 +124,7 @@ if __name__ == '__main__':
 	root = Tk()
 	root.title("serial tool")
 
-	serial_ui(root)
+	s=serial_ui(root)
+	s.comList.set(['COM1', 'COM2', 'COM3', 'COM4', 'COM5'])
 
 	root.mainloop()
